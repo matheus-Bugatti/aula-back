d6e3038a2b9a6007f97d2d008e8967384c068180
matheus-bugatti <campos.n@aluno.ifsp.edu.br>
2025-03-24 15:16:10 -0300

    mudanca no appjs

--- js/app.js
+++ js/app.js
@@ -24,37 +24,7 @@ const alunoService = new AlunoService()
 alunos.forEach(aluno => {
     alunoService.add(new AlunoModel(aluno))
 });
-//
 
 
-// Inserir no tbody da tabela a lista de alunos e suas médias
-
-
-render();
-
-// Inserir aluno
-// Padrão do HTML, o form quando tem um botão ele automaticamente quando clicado gera o submit
-document.querySelector('form').addEventListener('submit', function (event) {
-    //Por padrão quando ocorre um submit, ele atualiza a página
-    event.preventDefault()
-    //Pegar o valor de um elemento em especifico, o que foi escrito como nome do aluno
-    const nome = document.getElementById('first_name').value
-    const newAluno = {
-        _id: 0,
-        nome,
-        notas: {
-            backend_1: [6, 6, 6, 6],
-            frontend_2: [7, 7, 7, 7],
-            bancodados: [6, 6, 6, 6],
-            ferramentas: [7, 7, 7, 7],
-        }
-    }
-    newAluno.media = {}
-    for (let materia in newAluno.notas) {
-        newAluno.media[materia] = average(...newAluno.notas[materia])
-    }
-    alunos.push(newAluno)
-    render()
-})
-
-
+const alunoView = new AlunoView(document.querySelector('[data-table-alunos]'))
+const alunoController = new AlunoController(alunoService, alunoView)
