ce4840273cd8534d36234bc0a486dbfa74df86a7
matheus-bugatti <campos.n@aluno.ifsp.edu.br>
2025-03-24 16:51:16 -0300

    controler modificado sla

--- js/app.js
+++ js/app.js
@@ -28,3 +28,9 @@ alunos.forEach(aluno => {
 
 const alunoView = new AlunoView(document.querySelector('[data-table-alunos]'))
 const alunoController = new AlunoController(alunoService, alunoView)
+
+document.querySelector('form').addEventListener('submit', function (event) {
+    event.preventDefault()
+    const nome = document.getElementById('first_name').value
+    alunoController.add({ nome })
+})
--- js/controllers/Aluno.controller.js
+++ js/controllers/Aluno.controller.js
@@ -1,5 +1,11 @@
 class AlunoController {
     constructor(service, view) {
         view.render(service.alunos)
+        this.view = view
+        this.service = service
+    }
+    add(aluno) {
+        this.service.add(new AlnunoModel(aluno))
+        this.view.render(this.service.alunos)
     }
 }
